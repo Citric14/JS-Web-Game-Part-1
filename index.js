8130d18885f756e8fd3781e996446e1ead0e3f34
George <georgehender997@gmail.com>
2022-08-23 20:33:16 -0400

    finished

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ function newImage(url, left, bottom){
 
 
 newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
 
+
+
+function newItem(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    
+    object.addEventListener('dblclick', function() {
+        object.remove()
+    })
+}
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
 
 
